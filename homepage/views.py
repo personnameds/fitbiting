d48c6afdce767140647e13bee3fa510c225896e1
personnameds <personnameds@gmail.com>
2019-08-22 08:09:40 -0400

    Add Homepage Template Dir

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomepageView(TemplateView):
-	template_name="homepage.html"
+	template_name="homepage/homepage.html"
 
